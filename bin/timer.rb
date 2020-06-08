48316fb4a2fe0f8f9783cd98f2b9643c73238454
Glory David <glodave99@gmail.com>
2020-06-08 22:26:36 +0100

    add timer.rb

--- bin/timer.rb
+++ bin/timer.rb
@@ -2,27 +2,23 @@
 
 require 'telegram/bot'
 require 'dotenv/load'
-require_relative '../lib/entries.rb'
+require_relative '../lib/state_manager.rb'
 
-puts Dir.pwd
-file_data = File.read('../db/nuggets.txt').split("\n")
 
 token = ENV['TELEGRAM_API_KEY']
 
-loop do
-  users = Entries.users
-  # users = [1061110010]
-  i = 0
-  while i < users.size
-    user = users[i].to_i
+file_data = File.read('../db/nuggets.txt').split("\n")
 
+loop do
+  users = StateManager.items_managed('users')
+  users.each do |user|
     Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: user, text: "Word for the day:\n#{file_data[rand(1...file_data.size)]}")
     end
     p user
     sleep(1)
-    i += 1
   end
+  
   puts Time.now
   sleep(14_400)
 end
