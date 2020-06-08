8c71aa483563c21dd122f733643038677192ff3c
Glory David <glodave99@gmail.com>
2020-06-08 10:12:34 +0100

    add entries class to support timer db

---
+++ lib/entries.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class Entries
+  attr_reader :user, :file
+  @@users = []
+
+  def initialize(user)
+    @user = user
+    @file = '.db/users.txt'
+    @@users = update_arr(@user, @file)
+  end
+
+  def self.users
+    @@users = File.read(@file).split("\n")
+  end
+
+  def delete_user(use)
+    uses = File.read(@file).split("\n")
+    uses.delete(use.to_s)
+    n = File.new(@file, 'w')
+    uses.each do |item|
+      n.write("#{item}\n")
+    end
+    n.close
+  end
+
+  private
+  
+  def update_arr(user, file)
+    current_users = File.read(file).split("\n")
+    unless current_users.include? user.to_s
+      n = File.new(file, 'a')
+      n.write("#{user}\n")
+      n.close
+    end
+    current_users
+  end
+
+  # def save_entry(user, data)
+  #   entry_data[user.to_sym][Time.new.strftime('%Y%m%d%H%M%S').to_s.to_sym.to_sym] = data
+  # end
+end
