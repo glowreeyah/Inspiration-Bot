4b61387788289cd8202751a2a8f5e8f69d17fef1
Glory David <glodave99@gmail.com>
2020-06-08 16:09:19 +0100

    add Module to manage Database

---
+++ lib/database_manager.rb
@@ -0,0 +1,46 @@
+module DatabaseManager
+  def append_to_file(file_name, entry)
+    f = File.new("./db/#{file_name}.txt", 'a')
+    f.write("#{entry}\n")
+    f.close
+    'appended'
+  end
+
+  def file_to_arr(file_name)
+    File.read("./db/#{file_name}.txt").split("\n")
+  end
+
+  def overwrite_file(file_name, new_arr)
+    f = File.new("./db/#{file_name}.txt", 'w')
+    new_arr.each do |item|
+      f.write("#{item}\n")
+    end
+    f.close
+    'overwritten'
+  end
+
+  def contain_in_file?(file_name, entry)
+    file_to_arr(file_name).include?(entry.to_s) ? true : false
+  end
+
+  def remove_from_file(file_name, entry)
+    if contain_in_file?(file_name, entry)
+      temp_arr = file_to_arr(file_name)
+      temp_arr.delete(entry.to_s)
+      overwrite_file(file_name, temp_arr)
+      'removed'
+    else
+      'not found'
+    end
+  end
+end
+
+## Testing
+#  include DatabaseManager
+
+#  append_to_file('glowy', 521_103_554)
+#  file_to_arr('glowy')
+#  overwrite_file('glowy', %w[this works])
+#  puts contain_in_file?('glowy', 'her') # false
+#  puts contain_in_file?('glowy', 'she') # true
+#  puts remove_from_file('glowy', 'she')
